408a6aa7c9a7e9c2e46fdc9c21d9945f9f9d96f7
falstack <icesilt@outlook.com>
2019-05-11 09:01:11 +0800

    update: deploy script

---
+++ pm2.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Mahoo',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      watch: false,
+      ignore_watch: 'node_modules',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_staging: {
+        NODE_ENV: 'staging'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}
