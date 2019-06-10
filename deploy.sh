4a4132e7a544451763996890a8a11f2702d45b66
falstack <icesilt@outlook.com>
2019-06-10 10:07:58 +0800

    update: deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 # 当发生错误时中止脚本
 set -e
 
+# 切换分支
+git checkout master
+git pull origin dev
+
 # 构建
 npm run build
 npm run upload
@@ -12,3 +16,6 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push origin master
+
+# 切回分支
+git checkout dev
