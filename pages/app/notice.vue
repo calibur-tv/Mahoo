f4ba7fde854f7f66275382961f194caa36346b69
falstack <icesilt@outlook.com>
2019-09-30 15:04:41 +0800

    deploy

--- pages/app/notice.vue
+++ pages/app/notice.vue
@@ -21,8 +21,16 @@
         transform: scaleY(0.5);
       }
 
+      p {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 40px;
+        width: 40px;
+        margin: 0 auto;
+      }
+
       span {
-        display: block;
         font-size: 13px;
         color: $color-text-2;
       }
@@ -37,9 +45,14 @@
     }
 
     .ic-eit {
+      display: block;
       color: #fff;
       background-color: $color-yellow;
       border-radius: 50%;
+      width: 24px;
+      height: 24px;
+      line-height: 24px;
+      font-size: 18px;
     }
 
     .ic-good_fill {
@@ -48,6 +61,7 @@
 
     .ic-notice_fill {
       color: $color-blue;
+      font-size: 29px;
     }
   }
 }
@@ -56,20 +70,20 @@
 <template>
   <div id="app-notice">
     <div class="msg-panel clearfix">
-      <div>
-        <i class="iconfont ic-message_fill" />
+      <div @click="todo">
+        <p><i class="iconfont ic-message_fill" /></p>
         <span>回复我的</span>
       </div>
-      <div>
-        <i class="iconfont ic-eit" />
+      <div @click="todo">
+        <p><i class="iconfont ic-eit" /></p>
         <span>@我</span>
       </div>
-      <div>
-        <i class="iconfont ic-good_fill" />
+      <div @click="todo">
+        <p><i class="iconfont ic-good_fill" /></p>
         <span>收到的赞</span>
       </div>
-      <div>
-        <i class="iconfont ic-notice_fill" />
+      <div @click="todo">
+        <p><i class="iconfont ic-notice_fill" /></p>
         <span>系统通知</span>
       </div>
     </div>
@@ -98,6 +112,10 @@ export default {
   watch: {},
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    todo() {
+      this.$toast.info('todo')
+    }
+  }
 }
 </script>
