2369efc7c662024104018a9e11db734e00bb5a2f
falstack <icesilt@outlook.com>
2019-10-09 08:41:44 +0800

    deploy

---
+++ .nuxt/dist/client/3d76146942cf7b2098ce.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{702:function(e,n,t){"use strict";t.r(n);t(62);var c={asyncData:function(e){var n=e.redirect,t=e.route;n("".concat(t.path.replace(/\/$/,""),"/following"),t.query)}},r=t(16),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
--- pages/app/zone.vue
+++ pages/app/zone.vue
@@ -1,21 +1,107 @@
-<style lang="scss"></style>
+<style lang="scss">
+#app-zone {
+  .page-header {
+    position: relative;
+    padding: 0 15px 15px;
+    overflow: hidden;
+
+    .header-core {
+      background-color: transparent;
+    }
+
+    .background {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      filter: blur(30px);
+      z-index: -1;
+    }
+
+    .intro {
+      img {
+        width: 100px;
+        height: 100px;
+        border-radius: 5px;
+      }
+    }
+  }
+}
+</style>
 
 <template>
   <div id="app-zone">
-    app zone
+    <div class="page-header">
+      <Header />
+      <div class="background" :style="{ backgroundImage: `url(${$resize(tag.avatar, { width: 100 })})` }" />
+      <div class="intro">
+        <img :src="$resize(tag.avatar, { width: 100 })" />
+      </div>
+    </div>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
+    <p>123</p>
   </div>
 </template>
 
 <script>
+import Header from '~/components/app/Header'
+import { showTag } from '~/api/tagApi'
+
 export default {
   name: 'AppZone',
-  components: {},
+  components: {
+    Header
+  },
   props: {},
   data() {
-    return {}
+    return {
+      tag: null
+    }
   },
   computed: {},
   watch: {},
+  asyncData({ app, error, query }) {
+    const { slug } = query
+    return showTag(app, { slug })
+      .then(tag => {
+        return {
+          tag
+        }
+      })
+      .catch(error)
+  },
   created() {},
   mounted() {},
   methods: {}
