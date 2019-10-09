2298af9c319cacac136e37d59967332b1000f1fb
falstack <icesilt@outlook.com>
2019-10-09 14:13:32 +0800

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -200,6 +200,16 @@ module.exports = {
           }
         }
       }
+    },
+    scrollBehavior: function(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      }
+      let position = { x: 0, y: 0 }
+      if (to.hash) {
+        position = { selector: to.hash }
+      }
+      return position
     }
   },
 
